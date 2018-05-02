09f7f20119d4586c212bad6e45a0340679b589dd
Sachin Jadhav <34229084+theSachinJadhav@users.noreply.github.com>
2018-05-03 00:24:50 +0530

    server keys secured

--- config/keys.js
+++ config/keys.js
@@ -1,10 +1,10 @@
-module.exports = {
-  mongoURI: "mongodb://sachin:sachin@ds149279.mlab.com:49279/social-media",
-  secretOrKey: "secret"
-};
+// module.exports = {
+//   mongoURI: "mongodb://sachin:sachin@ds149279.mlab.com:49279/social-media",
+//   secretOrKey: "secret"
+// };
 
-// if (process.env.NODE_ENV === "production") {
-//   module.exports = require("./keys_prod");
-// } else {
-//   module.exports = require("./keys_dev");
-// }
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
