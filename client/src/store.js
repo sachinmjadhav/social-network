4770529712103090ab67a12cf20980bc9b816180
Sachin Jadhav <34229084+theSachinJadhav@users.noreply.github.com>
2018-05-08 18:41:36 +0530

    Redux error fix

--- client/src/store.js
+++ client/src/store.js
@@ -6,13 +6,17 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const composeEnhancer =
+  process.env.NODE_ENV !== "production" &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        name: "App",
+        actionBlacklist: ["REDUX_STORAGE_SAVE"]
+      })
+    : compose;
+
+const enhancer = composeEnhancer(applyMiddleware(...middleware));
+
+const store = createStore(rootReducer, initialState, enhancer);
 
 export default store;
